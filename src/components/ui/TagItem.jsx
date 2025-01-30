b0f3a9fd3d154e8aa56a4ac102e335ec96624adb
Derek Brown <dmb10vols@gmail.com>
2025-01-29 20:19:32 -0600

    Update styles

---
+++ src/components/ui/TagItem.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+export default function TagItem({ tag }) {
+  return (
+    <a href={`/tags/${tag}`}>
+      <div class="bg-[#1E262F] rounded-lg mr-3 text-lg flex items-center">
+        <div class="px-4 py-2">{tag}</div>
+      </div>
+    </a>
+  )
+}
