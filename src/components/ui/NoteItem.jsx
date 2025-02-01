65ec578706af723046b35f3d41284e34efd8ae69
Derek Brown <dmb10vols@gmail.com>
2025-01-31 21:50:16 -0600

    Update styles

--- src/components/ui/NoteItem.jsx
+++ src/components/ui/NoteItem.jsx
@@ -2,13 +2,13 @@ import moment from 'moment'
 
 export default function NoteItem({ note }) {
   const title = note.data.title
-  const pubDate = moment(note.data.pubDate).format('MMM D YYYY')
+  const publishedDate = moment(note.data.pubDate).format('MMM D YYYY')
 
   return (
     <a href={`/notes/${note.id}`}>
       <div class="flex flex-col justify-between md:flex-row md:mb-3 mb-3 bg-[#1E262F] p-6 text-lg rounded-lg">
         <div class="mr-2">{title}</div>
-        <div class="text-[#7E8C9A] shrink-0">{pubDate}</div>
+        <div class="text-[#7E8C9A] shrink-0">{publishedDate}</div>
       </div>
     </a>
   )
