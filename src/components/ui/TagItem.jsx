0e873db12b0fea1ba902144dbe66d286bbb2c23a
Derek Brown <dmb10vols@gmail.com>
2025-02-03 20:30:36 -0600

    Update styles

--- src/components/ui/TagItem.jsx
+++ src/components/ui/TagItem.jsx
@@ -1,7 +1,7 @@
 export default function TagItem({ tag }) {
   return (
     <a href={`/tags/${tag}`}>
-      <div class="bg-[#1E262F] rounded-lg mr-3 text-lg flex items-center">
+      <div class="bg-[#1E262F] rounded-lg mr-2 text-lg flex items-center">
         <div class="px-4 py-2">{tag}</div>
       </div>
     </a>
