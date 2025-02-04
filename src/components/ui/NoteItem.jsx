fad98759316fed15bd21df608fb525a765e31107
Derek Brown <dmb10vols@gmail.com>
2025-02-03 20:34:52 -0600

    Update styles

--- src/components/ui/NoteItem.jsx
+++ src/components/ui/NoteItem.jsx
@@ -6,7 +6,7 @@ export default function NoteItem({ note }) {
 
   return (
     <a href={`/notes/${note.id}`}>
-      <div class="flex flex-col justify-between md:flex-row md:mb-3 mb-3 bg-[#1E262F] p-6 text-lg rounded-lg">
+    <div class="flex flex-col justify-between md:flex-row md:mb-2.5 mb-2.5 bg-[#1E262F] p-6 text-lg rounded-lg">
         <div class="mr-2">{title}</div>
         <div class="text-[#7E8C9A] shrink-0">{publishedDate}</div>
       </div>
