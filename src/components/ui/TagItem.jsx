056668ebb9cfe57fdf5a565bb8c4c0cdc30bd149
Derek Brown <dmb10vols@gmail.com>
2026-04-28 21:00:24 -0500

    Update styles

--- src/components/ui/TagItem.jsx
+++ src/components/ui/TagItem.jsx
@@ -1,8 +1,11 @@
-export default function TagItem({ tag }) {
+export default function TagItem({ tag, count }) {
   return (
     <a href={`/tags/${tag}`}>
       <div class="bg-[#1E262F] rounded-lg mr-2.5 text-lg flex items-center">
         <div class="px-4 py-2">{tag}</div>
+        {count !== undefined && (
+          <div class="pr-4 text-[#7E8C9A] text-sm">{count}</div>
+        )}
       </div>
     </a>
   )
